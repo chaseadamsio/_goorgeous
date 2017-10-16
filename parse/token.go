a8f134d5034d296700ca68c5d826073b6a55f3b5
Chase Adams <cadams@walmartlabs.com>
2017-10-16 15:31:21 -0700

    add tokens for itemType

--- parse/token.go
+++ parse/token.go
@@ -10,13 +10,16 @@ const (
 	itemNewline
 	itemEOF
 	itemText
+	itemAsterisk // "*" indicates either a headline or a bold token
+	itemComment  // "#  " indicates a comment token
 )
 
 var itemTypes = [...]string{
-	itemError:   "itemError",
-	itemNewline: "itemNewline",
-	itemEOF:     "itemEOF",
-	itemText:    "itemText",
+	itemError:    "itemError",
+	itemNewline:  "itemNewline",
+	itemEOF:      "itemEOF",
+	itemText:     "itemText",
+	itemAsterisk: "*",
 }
 
 func (typ itemType) String() string {

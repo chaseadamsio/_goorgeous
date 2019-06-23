1253e87a87e46560d9d5836a9c8fafeeddb52351
Chase Adams <realchaseadams@gmail.com>
2019-06-22 23:21:44 -0700

    initial html transform

--- transform/html.go
+++ transform/html.go
@@ -2,35 +2,52 @@ package transform
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/chaseadamsio/goorgeous/ast"
 )
 
-func TransformToHTML(inAST *ast.RootNode) {
-	fmt.Println(inAST)
-	// fmt.Printf("found headline node: <h%d>%s</h%d>", hl.Depth, hl, hl.Depth)
+func TransformToHTML(root *ast.RootNode) string {
+	return walk(root.Children())
 }
 
-func flattenTree(inAST []ast.Node) []ast.Node {
-	return nil
+func walk(inAST []ast.Node) string {
+	var out []string
 
-	// for _, child := range inAST {
-	// 	if hl, ok := child.(*ast.HeadlineNode); ok {
-	// 		out := hl.ToJSON()
-	// 		// fmt.Printf("found headline node: <h%d>%s</h%d>", hl.Depth, hl, hl.Depth)
-	// 	}
-	// }
-	// var outAST []HTMLNode
-	// for _, child := range inAST {
-	// 	if _, found := nonHTML[string(child.Type())]; !found {
-	// 		outAST = append(outAST, HTMLNode{
-	// 			Type:  string(child.Type()),
-	// 			Value: child.String(),
-	// 		})
-	// 	}
-	// 	if len(child.Children()) > 0 {
-	// 		outAST = append(outAST, flattenTree(child.Children())...)
-	// 	}
-	// }
-	// return outAST
+	for _, child := range inAST {
+		switch node := child.(type) {
+		case *ast.HeadlineNode:
+			out = append(out, processHeadlineNode(node))
+		case *ast.ListNode:
+			out = append(out, processListNode(node))
+		case *ast.ListItemNode:
+			out = append(out, processListItemNode(node))
+		case *ast.TextNode:
+			out = append(out, node.Val)
+		default:
+		}
+	}
+
+	return strings.Join(out, "\n")
+}
+
+func processHeadlineNode(node *ast.HeadlineNode) string {
+	return fmt.Sprintf("<h%d>%s</h%d>", node.Depth, node.Children()[0], node.Depth)
+}
+
+func processListNode(node *ast.ListNode) string {
+	listTyp := ""
+	if node.ListType == "UNORDERED" {
+		listTyp = "ul"
+	}
+	if node.ListType == "ORDERED" {
+		listTyp = "ol"
+	}
+	children := walk(node.Children())
+	return fmt.Sprintf("<%s>%s</%s>", listTyp, children, listTyp)
+}
+
+func processListItemNode(node *ast.ListItemNode) string {
+	children := walk(node.ChildrenNodes)
+	return fmt.Sprintf("<li>%s</li>", children)
 }
--- transform/html_test.go
+++ transform/html_test.go
@@ -1,33 +1,65 @@
 package transform
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/chaseadamsio/goorgeous/parse"
+	"github.com/chaseadamsio/goorgeous/testdata"
+	"github.com/google/go-cmp/cmp"
 )
 
-type testCase struct {
-	input string
-	// expected string
-}
+var update = flag.Bool("update", false, "update golden files")
 
-var testCases = []testCase{
-	{
-		"* This is a headline",
-	},
+func snapshotPath(filename string) string {
+	return fmt.Sprintf("snapshots/%s.html", filename)
 }
 
 func TestTransform(t *testing.T) {
-	for _, tc := range testCases {
-		ast := parse.Parse(tc.input)
-		fmt.Println(ast)
-		// TransformToHTML(ast)
-		// flatOut := []string{"\n"}
+	for _, filename := range testdata.Tests {
 
-		// for _, node := range out {
-		// 	flatOut = append(flatOut, "type: "+string(node.Type)+"\n\t"+node.Value+"\n")
-		// }
-		// fmt.Println(strings.Join(flatOut, ""))
+		t.Run(filename, func(t *testing.T) {
+
+			value := testdata.GetOrgStr(filename)
+			ast := parse.Parse(value)
+
+			snapshotPath := snapshotPath(filename)
+
+			out := TransformToHTML(ast)
+			fmt.Println(out)
+
+			if *update {
+				err := os.MkdirAll(filepath.Dir(snapshotPath), os.ModePerm)
+				if err != nil {
+					t.Errorf("failed to make directories for %s: %s", snapshotPath, err)
+				}
+				if err := ioutil.WriteFile(snapshotPath, []byte(out), os.ModePerm); err != nil {
+					t.Errorf("failed to write %s file: %s", snapshotPath, err)
+				}
+				return
+			}
+
+			snapshot, err := ioutil.ReadFile(snapshotPath)
+			if err != nil {
+				t.Fatalf("failed to read %s file: %s", snapshotPath, err)
+			}
+
+			var expected map[string]interface{}
+			err = json.Unmarshal([]byte(snapshot), &expected)
+			if err != nil {
+				t.Errorf("failed to unmarshal expected string: %s", err)
+				// use an empty string to get a view of the world and uncomment this next line...
+				// fmt.Printf("\nexpected:\n\t%v\nactual:\n\t%v", expected, ast)
+			}
+
+			if cmp.Equal(ast, expected) {
+				t.Errorf("expected %s AST shape to match expected.", snapshotPath)
+			}
+		})
 	}
 }

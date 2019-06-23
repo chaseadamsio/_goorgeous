5ea9d518de2caac87269691223fd13c43fc30070
Chase Adams <realchaseadams@gmail.com>
2019-06-22 23:21:34 -0700

    move Tests list to testdata

--- parse/parse_test.go
+++ parse/parse_test.go
@@ -16,11 +16,11 @@ import (
 var update = flag.Bool("update", false, "update golden files")
 
 func snapshotPath(filename string) string {
-	return fmt.Sprintf("snapshots/%s", filename)
+	return fmt.Sprintf("snapshots/%s.json", filename)
 }
 
 func TestParse(t *testing.T) {
-	for _, filename := range tests {
+	for _, filename := range testdata.Tests {
 		// filter := "headline/headline-1.org"
 		// if !strings.HasPrefix(filename, filter) {
 		// 	continue
@@ -64,46 +64,4 @@ func TestParse(t *testing.T) {
 		})
 	}
 
-}
-
-type testCase struct {
-	source string
-	golden string
-}
-
-var tests = []string{
-	testdata.ElementPlain,
-	testdata.ElementNested,
-	testdata.ElementBold,
-	testdata.ElementHorizontalRule,
-	testdata.Headline1,
-	testdata.Headline1And2,
-	testdata.Headline1WithContent,
-	testdata.HeadersBasic,
-	testdata.LinkStandard,
-	testdata.LinkSelfDescriptive,
-	testdata.LinkBoth,
-	testdata.OrderedListBasic,
-	testdata.OrderedListNotAList,
-	testdata.OrderedListWithStartingNewline,
-	testdata.OrderedListFollowParagraph,
-	testdata.OrderedListFollowNumberNotList,
-	testdata.OrderedListFollowAsteriskHeading,
-	testdata.OrderedListWithFollowUnOrderedList,
-	testdata.OrderedListWithNestedOrderedList,
-	testdata.OrderedListWithNestedUnorderedList,
-	testdata.OrderedListWithDeepNestedChildren,
-	testdata.OrderedListWithNestedContent,
-	testdata.TableBasic,
-	testdata.UnorderedListBasic,
-	testdata.UnorderedListNotAList,
-	testdata.UnorderedListWithStartingNewline,
-	testdata.UnorderedListFollowParagraph,
-	testdata.UnorderedListFollowDashNotList,
-	testdata.UnorderedListFollowAsteriskHeading,
-	testdata.UnorderedListWithFollowOrderedList,
-	testdata.UnorderedListWithNestedOrderedList,
-	testdata.UnorderedListWithNestedUnorderedList,
-	testdata.UnorderedListWithDeepNestedChildren,
-	testdata.UnorderedListWithNestedContent,
 }
